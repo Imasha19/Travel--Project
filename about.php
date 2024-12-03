7590ef26c052dea87230f191dabb165ec9da095d
Imasha19 <imashasandanayaka@gmail.com>
2024-12-03 23:15:23 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!--swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+
+    <!--custom css file link-->
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+    
+<!--header section starts-->
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+</section>
+
+
+<!--header section ends-->
+
+<div class="heading" style="background:url(images/aboutnew.jpg) no-repeat">
+    <h1>about us</h1>
+
+</div>
+
+<!--about section starts-->
+ 
+<section class="about">
+
+    <div class="image">
+       <img src="images/about4.jpg" alt="">
+    </div>
+ 
+    <div class="content">
+       <h3>why choose us?</h3>
+       <p>At Master's travel, we are dedicated to turning your travel dreams into unforgettable experiences. With years of expertise in curating unique itineraries, we ensure every trip is tailored to your preferences, whether you’re seeking adventure, relaxation, or cultural exploration. Our team of passionate travel experts works tirelessly to craft journeys that combine exceptional value with extraordinary destinations, ensuring every moment is filled with wonder and discovery.</p>
+    
+       <div class="icon-container">
+          <div class="icons">
+              <i class="fas fa-map"></i>
+              <span>top destinations</span>
+          </div>   
+
+          <div class="icons">
+              <i class="fas fa-hand-holding-usd"></i>
+              <span>affordable price</span>
+          </div> 
+
+          <div class="icons">
+              <i class="fas fa-headset"></i>
+              <span>24/7 guide service</span>
+          </div> 
+   
+      </div>
+   
+      </div>  
+</section>
+
+<!--about section ends-->
+
+<!--reviews section starts-->
+
+<section class="reviews">
+<h1>CLIENT REVIEWS</h1>
+    <div class="reviews-slider">
+       
+       <div class="wrapper">
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+              </div>
+              <p>I had an incredible experience planning my trip with this website! The process was seamless, the recommendations were spot-on, and the customer service was outstanding. I’ll definitely use it again for my next adventure!</p>
+              <h3>John Mac</h3>
+              <span>traveler</span>
+              <img src="images/profile1.jpeg" alt="">
+          </div>
+
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 
+              </div>
+              <p>The website's comprehensive information on destinations, accommodations, and activities was invaluable in helping me curate the perfect itinerary. I couldn't be happier with my choice!</p>
+              <h3>Shiwani Kamalini</h3>
+              <span>traveler</span>
+              <img src="images/profile2.jpeg" alt="">
+          </div>
+
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                
+                 
+              </div>
+              <p>I highly recommend Master's travel to anyone seeking a hassle-free and enjoyable travel experience. Their commitment to customer satisfaction is truly commendable.</p>
+              <h3>Mohomad Ismail</h3>
+              <span>traveler</span>
+              <img src="images/profile3.jpeg" alt="">
+          </div>
+
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+              </div>
+              <p>This travel website made my vacation stress-free and unforgettable! The personalized itineraries and great deals exceeded my expectations. Highly recommend it to anyone planning their next getaway!</p>
+              <h3>Jenny Williyam</h3>
+              <span>traveler</span>
+              <img src="images/profile4.jpeg" alt="">
+          </div>
+
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 
+              </div>
+              <p>Booking through this website was a breeze! The team’s attention to detail and quick support made my trip absolutely perfect. I couldn’t have asked for a better experience!</p>
+              <h3>Sanjeewa Fernando</h3>
+              <span>traveler</span>
+              <img src="images/profile5.jpeg" alt="">
+          </div>
+
+          <div class=" slide">
+             <div class="stars">
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 <i class="fas fa-star"></i>
+                 
+              </div>
+              <p>Amazing service and great value! The recommendations were fantastic, and everything was perfectly organized from start to finish. I’m already planning my next trip with them!</p>
+              <h3>Takumi Sora</h3>
+              <span>traveler</span>
+              <img src="images/profile6.jpeg" alt="">
+          </div>
+
+        </div>
+
+     </div>
+
+
+
+</section>
+
+
+<!--reviews secion ends-->
+
+
+
+
+
+
+
+
+
+<!--footer section starts-->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+        <h3>quick links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i>home</a>
+        <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+        <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+        <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+      </div>
+
+      <div class="box">
+        <h3>extra links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i>ask questions</a>
+        <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+        <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+        <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+      </div>
+
+      <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i>+123-456-7890</a>
+        <a href="#"> <i class="fas fa-phone"></i>+111-222-3333</a>
+        <a href="#"> <i class="fas fa-envelope"></i>shashika@gmail.com</a>
+        <a href="#"> <i class="fas fa-map"></i>No 24,Kandy road,Malabe</a>
+      </div>
+
+      <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i>facebook</a>
+        <a href="#"> <i class="fab fa-twitter"></i>twitter</a>
+        <a href="#"> <i class="fab fa-instagram"></i>instagram</a>
+        <a href="#"> <i class="fab fa-linkedin"></i>linkedin</a>
+
+
+    </div>
+
+  <div class="credit"> created by <span>I K S web designer</span> | All rights reserved! </div>
+
+</section>
+
+
+<!--footer section ends-->
+
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!--custom js file link-->
+<script src="script.js"></script>
+
+
+
+</body>
+</html>
